11eaa39ce504fc24f27bb39ff787325381a2aac8
Michael Smith <MSmith@soboba-nsn.gov>
2020-02-26 11:08:40 -0800

    Fixing additional forecast data. Called wrong array

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -16,7 +16,7 @@ const forecast = (latitude, longitude, callback) =>{
             callback('Unable to find location', undefined)
         } else {
             callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. ' + 
-            'The low for the day is ' + body.daily.data[13] + '. The high for the day is ' + body.daily.data[11] + '.')
+            'The low for the day is ' + body.daily.data[0].temperatureLow + '. The high for the day is ' + body.daily.data[0].temperatureHigh + '.')
         }
     })
 }
